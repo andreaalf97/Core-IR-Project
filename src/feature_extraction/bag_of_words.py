5c1ede98eaaf2800edaf191473de47ed4d680d96
Avinash Saravanan <asarav@umich.edu>
2020-03-29 13:17:05 +0200

    Include beginnings of bag_of_words logic

--- src/feature_extraction/bag_of_words.py
+++ src/feature_extraction/bag_of_words.py
@@ -9,9 +9,15 @@ for i in range(loader.start, loader.end):
     loader.load_file_data(incrementCurrentIndex=True)
     tables = loader.get_table_ids(data=loader.currentData)
     #Use an inner loop to get the individual data
-    #print(len(tables))
     for j in range(0, len(tables)):
-        print(j)
-        id = tables[j]
-        if "title" in loader.currentData[id]:
-            print(loader.currentData[id].title)
+        tableID = tables[j]
+        if 'title' in loader.currentData[tableID]:
+            print(loader.currentData[tableID]['title'])
+        if 'pgTitle' in loader.currentData[tableID]:
+            print(loader.currentData[tableID]['pgTitle'])
+        if 'caption' in loader.currentData[tableID]:
+            print(loader.currentData[tableID]['caption'])
+        if 'secondTitle' in loader.currentData[tableID]:
+            print(loader.currentData[tableID]['secondTitle'])
+        if 'data' in loader.currentData[tableID]:
+            print(loader.currentData[tableID]['data'])
