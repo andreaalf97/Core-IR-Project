f526cc5e29749b44bb0e0a9beed87cab2aab297f
Avinash Saravanan <asarav@umich.edu>
2020-03-29 13:12:39 +0200

    Update examples

--- src/feature_extraction/bag_of_words.py
+++ src/feature_extraction/bag_of_words.py
@@ -3,10 +3,15 @@ import pandas as pd
 
 loader = data_loader.data_loader()
 
+df = pd.DataFrame()
+
 for i in range(loader.start, loader.end):
     loader.load_file_data(incrementCurrentIndex=True)
     tables = loader.get_table_ids(data=loader.currentData)
     #Use an inner loop to get the individual data
+    #print(len(tables))
     for j in range(0, len(tables)):
+        print(j)
         id = tables[j]
-        print(loader.currentData[id])
+        if "title" in loader.currentData[id]:
+            print(loader.currentData[id].title)
