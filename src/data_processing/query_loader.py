321cfd94ebb44234eceae161938a9f0901cfa93b
Avinash Saravanan <asarav@umich.edu>
2020-04-02 18:58:08 +0200

    Add logic to load data from query txt files

---
+++ src/data_processing/query_loader.py
@@ -0,0 +1,19 @@
+import json
+from pandas import json_normalize
+
+class query_loader:
+    fileLocation = "../data/query_data/queries.txt"
+    # Data will have the following format: an array containing an array with 2 elements.
+    # The first element will contain the query number. The second element will contain the individual words for the query
+    data = []
+
+    def __init__(self):
+        # Read from txt file
+        f = open(self.fileLocation, "r")
+        # Split into elements by \n
+        unparsedData = str.splitlines(f.read())
+        for i in range(0, len(unparsedData)):
+            # Split by spaces
+            splitStrings = unparsedData[i].split(' ')
+            row = [splitStrings[0], splitStrings[1:]]
+            self.data.append(row)
