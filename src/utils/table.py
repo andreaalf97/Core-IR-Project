df13cfe6b9f2569263b9d670b9b5f4fd982b88e4
andreaalf97 <andrea.alfieri97@gmail.com>
2020-04-07 18:09:35 +0200

    completed IDF feature extraction

--- src/utils/table.py
+++ src/utils/table.py
@@ -3,8 +3,9 @@ from src.data_processing.data_loader import data_loader
 class Table:
     '''This class is used to represent a table object and its parameters'''
 
-    def __init__(self, pageTitle, sectionTitle, tableCaption, tableHeadings, tableBody):
+    def __init__(self, tableName, pageTitle, sectionTitle, tableCaption, tableHeadings, tableBody):
         '''To initialize this class manually'''
+        self.tableName = tableName
         self.pageTitle = pageTitle
         self.sectionTitle = sectionTitle
         self.tableCaption = tableCaption
@@ -12,10 +13,37 @@ class Table:
         self.tableBody = tableBody
 
     # Still working on this
-    def __init__(self, jsonData):
+    def __init__(self, tableName, jsonData):
         '''To initialize this class from a JSON file'''
-        self.pageTitle = jsonData
-        self.sectionTitle = jsonData
-        self.tableCaption = jsonData
-        self.tableHeadings = jsonData
-        self.tableBody = jsonData
+        self.tableName = tableName
+        self.pageTitle = jsonData["pgTitle"]
+        self.sectionTitle = jsonData["secondTitle"]
+        self.tableCaption = jsonData["caption"]
+        self.tableHeadings = jsonData["title"]
+
+        self.tableBody = jsonData["data"]  # Please note that each element in this array is a ROW
+
+    def getTableAsString(self):
+        result = ""
+        result += self.pageTitle + " "
+        result += self.sectionTitle + " "
+        result += self.tableCaption + " "
+
+        for w in self.tableHeadings:
+            result += w + " "
+
+        for i in range(len(self.tableBody)):
+            for j in range(len(self.tableBody[i])):
+                result += self.tableBody[i][j] + " "
+
+        return result
+
+
+if __name__ == '__main__':
+    dl = data_loader()  # Instantiate the data loader class
+    data = dl.load_preprocessed_data()  # Loads the clean data
+    for v in data:
+        table = Table(v, data[v])  # Creates a Table object from the JSON file
+        break
+
+    #print(table.tableBody)  # Testing if the values are in place
