a29a32bd34e5bf14f08839366922b3e4ae3d2060
Avinash Saravanan <asarav@umich.edu>
2020-04-08 10:46:23 +0200

    Create class that is used for loading from dbPedia using SPARQL

---
+++ src/data_processing/dbPedia_entity_loader.py
@@ -0,0 +1,42 @@
+import requests
+import re
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+# This is a class used for handling entities from dbPedia.
+# Uses the following api
+class dbPediaEntityLoader:
+    def __init__(self):
+        self.S = requests.Session()
+        self.sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+
+    def match_urls(self, cell):
+        return re.match(r'\[(\w*)\|(.*)\]', cell)
+
+    def get_dbpedia_entities_by_cell(self, cell):
+        match = self.match_urls(cell)
+        if match:
+            self.sparql.setReturnFormat(JSON)
+
+            # This query gets the top 10 rdf:type values of an entity
+            query = '''SELECT DISTINCT ?s ( bif:search_excerpt ( bif:vector () , ?o ) ) WHERE {
+                ?s ?p ?o .
+                FILTER ( bif:contains ( ?o, '"''' + match.group(1) + '''"' ) )
+            }
+            LIMIT 10'''
+            self.sparql.setQuery(query)
+
+            # Map results to a list of types
+            entity = self.sparql.query().convert()
+            types = map(lambda x: x['s']['value'], entity['results']['bindings'])
+            return list(types)
+
+    def get_core_column_entities(self, table):
+        if len(table) == 0:
+            return []
+        column_entities = [[] for x in range(len(table[0]))]
+        for i in range(0, len(table)):
+            for j in range(0, len(table[i])):
+                cell_entities = self.get_dbpedia_entities_by_cell(table[i][j])  # Get entities of a cell
+                if cell_entities:
+                    column_entities[j].append(cell_entities)  # Append entities to current column
+        return max(column_entities, key=len)  # Return core column with most entities
--- src/feature_extraction/bag_of_entities.py
+++ src/feature_extraction/bag_of_entities.py
@@ -1,43 +1,12 @@
 from src.data_processing import data_loader
 import pandas as pd
-import re
-from SPARQLWrapper import SPARQLWrapper, JSON
+from src.data_processing import dbPedia_entity_loader
 
 loader = data_loader.data_loader()
+dbPediaLoader = dbPedia_entity_loader.dbPediaEntityLoader()
 
 df = pd.DataFrame()
 
-
-def match_urls(cell):
-    return re.match(r'\[(\w*)\|(.*)\]', cell)
-
-
-def get_dbpedia_entities(cell):
-    match = match_urls(cell)
-    if match:
-        sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-        sparql.setReturnFormat(JSON)
-
-        # This query gets the top 10 rdf:type values of an entity
-        query = 'SELECT DISTINCT ?o WHERE { dbr:' + match.group(1) + ' ?p ?o } LIMIT 20'
-        sparql.setQuery(query)
-
-        # Map results to a list of types
-        entity = sparql.query().convert()
-        types = map(lambda x: x['o']['value'], entity['results']['bindings'])
-        return list(types)
-
-
-def get_core_column_entities(table):
-    if len(table) == 0:
-        return []
-    column_entities = [[] for x in range(len(table[0]))]
-    for i in range(0, len(table)):
-        for j in range(0, len(table[i])):
-            cell_entities = get_dbpedia_entities(table[i][j])  # Get entities of a cell
-            if cell_entities:
-                column_entities[j].append(cell_entities)  # Append entities to current column
-    return max(column_entities, key=len)  # Return core column with most entities
 
 
 for i in range(loader.start, loader.end):
@@ -46,6 +15,7 @@ for i in range(loader.start, loader.end):
     #Use an inner loop to get the individual data
     for j in range(0, len(tables)):
         tableID = tables[j]
+        '''
         if 'title' in loader.currentData[tableID]:
             print(loader.currentData[tableID]['title'])
         if 'pgTitle' in loader.currentData[tableID]:
@@ -54,5 +24,6 @@ for i in range(loader.start, loader.end):
             print(loader.currentData[tableID]['caption'])
         if 'secondTitle' in loader.currentData[tableID]:
             print(loader.currentData[tableID]['secondTitle'])
+        '''
         if 'data' in loader.currentData[tableID]:
-            print(get_core_column_entities(loader.currentData[tableID]['data']))
+            print(dbPediaLoader.get_core_column_entities(loader.currentData[tableID]['data']))
