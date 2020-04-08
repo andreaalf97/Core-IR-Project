e83f88efbfdb94aeac95cb894cb87730325abaaa
andreaalf97 <andrea.alfieri97@gmail.com>
2020-04-08 19:25:24 +0200

    still working on word embeddings

--- src/feature_extraction/wordEmbeddings.py
+++ src/feature_extraction/wordEmbeddings.py
@@ -1,12 +1,13 @@
 from src.utils.globalLoaders import getQueryList, getTableList
-from src.data_processing.relevance_loader import RelevanceLoader
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from src.utils.table import Table
 import spacy
+import pandas as pd
 
 
-def normalize_string(string, lemmatizer: WordNetLemmatizer, remove_stopwords=True) -> list:
+def normalize_string(string, lemmatizer: WordNetLemmatizer, remove_stopwords=True, remove_duplicates=True) -> list:
     '''This function removes stopwords and lemmatizes the input string, then returns the '''
 
     # This is because queries will be already tokenized, but tables are just strings
@@ -21,12 +22,35 @@ def normalize_string(string, lemmatizer: WordNetLemmatizer, remove_stopwords=Tru
 
     string = [lemmatizer.lemmatize(word) for word in string]  # Lemmatize all words
 
+    if remove_duplicates:
+        string = list(dict.fromkeys(string))
+
     return string
 
 
-def word_embedding(word: str, nlp):
+def word_embedding(words: list, nlp):
     '''This functions receives a word and return its vector representation based on a pre-loaded dataset'''
-    return nlp(word).vector
+    return [nlp(word) for word in words]
+
+
+def getQueryByNumber(id: str, queries: list) -> str:
+    '''This function returns the query from its id'''
+
+    for query in queries:
+        if query[0] == str(id):
+            return " ".join(query[1])
+
+    raise Exception("The list of queries does not contain the give id:", id)
+
+
+def getTableById(id: str, tables: list) -> str:
+    '''This function returns a table from its id'''
+
+    for table in tables:
+        if table.tableName == id:
+            return table.getTableAsString()
+
+    raise Exception("The table with id (", id, ") was not found")
 
 
 if __name__ == '__main__':
@@ -37,17 +61,39 @@ if __name__ == '__main__':
     print("Finished loading")
 
     tables = getTableList()
-    queries = getQueryList()
+    queries: list = getQueryList()
 
-    words = queries[0][1]
-    print(words)
+    with open("../resources/extracted_features/features.csv", "r") as file:
+        df = pd.read_csv(file)
 
-    print("Normalizing...")
-    words = normalize_string(words, lemmatizer)
-    print(words)
+    lastQueryNumber = -1
+    for index, row in df.iterrows():
 
-    print("Vector representation:")
-    w2v = word_embedding("hello", nlp)
-    print(words, "-->", w2v)
-    print("Vector of size", len(w2v))
+        if row["queryNumber"] != lastQueryNumber:
+            query: str = getQueryByNumber(row["queryNumber"], queries)  # Retrieve the query from the id as a string
+            query_vectors: list = word_embedding(  # Extract the word embeddings from the normalized sentence
+                normalize_string(query, lemmatizer),
+                nlp
+            )
+
+        table: str = getTableById(row["tableId"], tables)
+
+        print("QUERY:", query)
+        print("TABLE no duplicates:", len(normalize_string(table, lemmatizer)))
+
+
+
+        break
+
+    # words = queries[0][1]
+    # print(words)
+    #
+    # print("Normalizing...")
+    # words = normalize_string(words, lemmatizer)
+    # print(words)
+
+    # print("Vector representation:")
+    # w2v = word_embedding("hello", nlp)
+    # print(words, "-->", w2v)
+    # print("Vector of size", len(w2v))
 
