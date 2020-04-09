a5c22a68d5fdc9cfffda53c24d40191e3f15fc07
andreaalf97 <andrea.alfieri97@gmail.com>
2020-04-09 15:39:13 +0200

    completed similarity measures and added word embeddings features to file

--- src/utils/similarity.py
+++ src/utils/similarity.py
@@ -1,23 +1,45 @@
 import numpy as np
 from src.utils.cos_similarity import cosine_similarity
+from spacy.tokens.token import Token
+from numpy import ndarray
 
 
 def early_fusion(table_terms: list, query_terms: list, vector_size=300) -> float:
+    '''
+    DO NOT use this function directly, it is only part of 'fusion' function
+    This function computes the early similarity measure.
+    table_terms and query_terms must be of type numpy.ndarray
+    '''
     table_avg = np.zeros(vector_size)
     for term in table_terms:
-        table_avg += term.vector
+        table_avg += term
 
     table_avg /= len(table_terms)
 
     query_avg = np.zeros(vector_size)
     for term in query_terms:
-        query_avg += term.vector
+        query_avg += term
 
     query_avg /= len(query_terms)
     return cosine_similarity(table_avg, query_avg)
 
 
 def fusion(table_terms: list, query_terms: list, vector_size=300) -> dict:
+    '''
+    This function returns the similarity of the two lists using different measures
+    Returns a dictionary with keys ["early", "late-max", "late-sum", "late-avg"]
+    '''
+
+    if type(table_terms[0]) == Token:
+        table_terms = [term.vector for term in table_terms]
+
+    if type(query_terms[0]) == Token:
+        query_terms = [term.vector for term in query_terms]
+
+    if type(table_terms[0] != ndarray):
+        raise Exception("table_terms should be of type ndarray")
+    if type(query_terms[0] != ndarray):
+        raise Exception("query_terms should be of type ndarray")
 
     similarities: dict = {
         "early": early_fusion(table_terms, query_terms, vector_size=vector_size),
@@ -33,14 +55,14 @@ def fusion(table_terms: list, query_terms: list, vector_size=300) -> dict:
     for table_term in table_terms:
         for query_term in query_terms:
 
-            if np.linalg.norm(table_term.vector) == 0:
+            if np.linalg.norm(table_term) == 0:
                 print(table_term)
                 exit(-1)
-            if np.linalg.norm(query_term.vector) == 0:
+            if np.linalg.norm(query_term) == 0:
                 print(query_term)
                 exit(-1)
 
-            similarity = cosine_similarity(table_term.vector, query_term.vector)
+            similarity = cosine_similarity(table_term, query_term)
             sum += similarity
             counter += 1
             if similarity > max_sim:
