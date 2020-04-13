5e8a8e9df1389f7607bb36c1135a7f8bc66b9b94
Avinash Saravanan <asarav@umich.edu>
2020-04-13 12:40:12 +0200

    Implement train test split

--- src/training/model.py
+++ src/training/model.py
@@ -1,12 +1,13 @@
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import ndcg_score
-from sklearn.model_selection import cross_validate
+from sklearn.metrics import ndcg_score, f1_score, explained_variance_score, max_error, mean_squared_error
+from sklearn.model_selection import cross_validate, train_test_split
 import numpy as np
 
 
 class Model:
     def __init__(self, data=[]):
         self.data = data
+        self.extractFeaturesAndLabels()
 
     def extractFeaturesAndLabels(self):
         # Construct features array
@@ -15,13 +16,21 @@ class Model:
         #Remove labels from features. We also remove any strings because they can't be converted to floats.
         #TODO: Maybe we should remove the query number as well?
         self.features = self.features.drop(["relevance", "tableId", "queryContents"], axis=1)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.features, self.labels,
+                                                                                test_size=0.2, random_state=0,
+                                                                                stratify=self.labels)
 
     def train(self):
         self.clf = RandomForestRegressor(max_depth=5, min_samples_split=3, random_state=0, oob_score=True)
         results = cross_validate(self.clf, self.features, self.labels, cv=5, verbose=1,
                            scoring=['explained_variance', 'max_error', "neg_mean_squared_error"], n_jobs=-1)
-        self.clf.fit(self.features, self.labels)
-        return results
+        self.clf.fit(self.X_train, self.y_train)
+        test_predictions = self.clf.predict(self.X_test)
+        testExplainedVarianceScore = explained_variance_score(self.y_test, test_predictions)
+        testMaxError = max_error(self.y_test, test_predictions)
+        testMeanSquaredError = mean_squared_error(self.y_test, test_predictions)
+        test_results = { 'variance_score': testExplainedVarianceScore, 'test_max_error': testMaxError, 'test_mean_squared_error': testMeanSquaredError }
+        return results, test_results
 
     def predict(self, item):
         cls =self.clf.predict(item)
@@ -54,4 +63,4 @@ class Model:
             data = self.data.loc[(self.data['tableId'] == rankings[i][0]) & (self.data['queryNumber'] == queryNumber)]
             trueScores.append(rankings[i][1])
             targetScores.append(int(data["relevance"].iat[0]))
-        return ndcg_score(y_true=np.asarray([trueScores]), y_score=np.asarray([targetScores]))
+        return ndcg_score(y_true=np.asarray([trueScores]), y_score=np.asarray([targetScores]), k=k)
--- src/training/train_model.py
+++ src/training/train_model.py
@@ -4,11 +4,16 @@ import pandas as pd
 
 df = pd.read_csv("../resources/extracted_features/features.csv")
 model = Model(data=df)
-model.extractFeaturesAndLabels()
-scores = model.train()
+scores, test_results = model.train()
 print(scores)
+print(test_results)
+all_scores = []
 for i in range(1, 61):
     print(i)
     ranking = model.getRankingForQuery(i)
     print(ranking)
-    print(model.ndcg_scoring(ranking, i))
+    ndgc_score = model.ndcg_scoring(ranking, i)
+    print(ndgc_score)
+    all_scores.append(ndgc_score)
+
+print(sum(all_scores) / len(all_scores))
