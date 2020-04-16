a50d8626da405195e75f015822907395438eb96e
Jasper van Esveld <j.m.vanesveld@student.tudelft.nl>
2020-04-16 15:26:02 +0200

    Refactor graph embeddings

--- src/feature_extraction/graphEmbeddings.py
+++ src/feature_extraction/graphEmbeddings.py
@@ -16,8 +16,6 @@ relevance = RelevanceLoader()
 table_data = loader.load_preprocessed_data()
 
 currentQuery = 0
-df = pd.DataFrame()
-queryDf = pd.DataFrame()
 ts = time.time()
 
 early = []
@@ -26,13 +24,12 @@ late_sum = []
 late_avg = []
 table_vectors_dict: dict = {}
 
-print("Loading w2v dataset...")
-# Loads the word embeddings model
-# en_core_web_lg is the largest word embeddings core model from spacy
+print("Loading DB2Ve dataset...")
 nlp = Word2Vec.load("../resources/datasets/DB2Vec_cbow_500_5_5_2_500", mmap="r")
 print("Finished loading")
 
 
+# Gets the graph embeddings of given entities from loaded DB2Vec data
 def get_graph_embeddings(entities):
     prefix = "http://dbpedia.org/resource/Category:"
     bagUtils.replace_prefix(entities, prefix, 'dct:')
@@ -40,6 +37,8 @@ def get_graph_embeddings(entities):
     bagUtils.replace_prefix(entities, prefix, 'dbr:')
     return [np.asarray(nlp.wv[entity]) for entity in entities if entity in nlp.wv.vocab]
 
+
+# Gets the graph embeddings of a table
 def extract_table_graph_embeddings(table):
     tableVector = []
     if 'pgTitle' in table:
@@ -59,28 +58,34 @@ def extract_table_graph_embeddings(table):
         tableVector += get_graph_embeddings(entities)
     return tableVector
 
+
 # Get json data
 # Iterate through query table pairs
 for i in range(0, len(relevance.data)):
     if currentQuery is not int(relevance.data[i][0]):
-        del queryDf
-        queryDf = pd.DataFrame()
         currentQuery = int(relevance.data[i][0])
+
+        # Print progress
         print(currentQuery)
         ts = time.time()
         print(datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+
+        # Extract query entities
         queryTerms = bagUtils.getQueryTerms(currentQuery)
         entities = []
         for j in range(0, len(queryTerms)):
-            # Use a higher limit to ensure that you get as many entities as possible for matching.
             entities += dbPediaLoader.get_entity_robust(queryTerms[j], limit=8, excludeCategories=False, onlyDbpedia=True)
         entities = bagUtils.remove_duplicates_from_list(entities)
-        queryVector:list = get_graph_embeddings(entities)
+
+        # Extract query embeddings from entities
+        queryVector = get_graph_embeddings(entities)
 
     tableId = relevance.data[i][1]
+    # Use stored embeddings if available
     if tableId in table_vectors_dict:
         tableVector = table_vectors_dict[tableId]
     else:
+        # Else get the embeddings and store them
         table = table_data[tableId]
         tableVector = extract_table_graph_embeddings(table)
         table_vectors_dict[tableId] = tableVector
